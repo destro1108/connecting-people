7e2c068489a7059df186aea23e5c7761f7cb11e1
destro1108 <swanandev@gmail.com>
2022-08-10 03:00:07 +0530

    feat: :art: add user btn, load sample button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <div className="mx-auto container h-screen w-screen">Initial</div>;
+  return (
+    <div className="container mx-auto h-full w-screen bg-outer-space-900 text-gray-200 font-Ubuntu">
+      <div className="w-full flex items-center gap-8 mt-20 px-3 py-2">
+        <div className="btn-outlined">Load Sample Data</div>
+        <div className="btn-contained">Add User</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
