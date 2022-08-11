606e6ffaf6f1e3de13e68892339a98b3c7487b72
destro1108 <swanandev@gmail.com>
2022-08-11 15:20:36 +0530

    feat: :sparkles: connecting people

--- src/components/peopleTable.tsx
+++ src/components/peopleTable.tsx
@@ -1,21 +1,58 @@
 import { Dispatch } from "react";
+import { relationsType } from "../lib";
 import { ACTIONS, ActionTypes } from "../reducer";
 import Person, { PersonType } from "./person";
 
-interface UserTableProps {
-  people: { [key: string]: PersonType };
+interface PeopleTableProps {
+  people: { [key: number]: PersonType };
   dispatch: Dispatch<ActionTypes>;
 }
 
-const UserTable = ({ people, dispatch }: UserTableProps) => {
+const PeopleTable = ({ people, dispatch }: PeopleTableProps) => {
   const deletePerson = (id: number) => dispatch({ type: ACTIONS.REMOVE_USER, payload: id });
   return (
     <div className="mt-6 px-2 flex flex-col border-2 border-slate-700 gap-0 divide-y-2 divide-slate-700">
-      {Object.entries(people).map(([id, person]) => (
-        <Person key={id} person={person} deletePerson={() => deletePerson(parseInt(id, 10))} />
-      ))}
+      {Object.keys(people).length ? (
+        Object.entries(people).map(([id, person]) => (
+          <Person
+            key={id}
+            id={parseInt(id, 10)}
+            people={people}
+            person={person}
+            deletePerson={() => deletePerson(parseInt(id, 10))}
+            connect={(relation: relationsType, personToId: number) => {
+              if (person.connections.some((conn) => conn.id === personToId)) return;
+              dispatch({
+                type: ACTIONS.UPDATE_USER,
+                payload: {
+                  id: parseInt(id, 10),
+                  person: {
+                    ...person,
+                    connections: [...person.connections, { relation, id: personToId }],
+                  },
+                },
+              });
+              dispatch({
+                type: ACTIONS.UPDATE_USER,
+                payload: {
+                  id: personToId,
+                  person: {
+                    ...people[personToId],
+                    connections: [
+                      ...people[personToId].connections,
+                      { relation, id: parseInt(id, 10) },
+                    ],
+                  },
+                },
+              });
+            }}
+          />
+        ))
+      ) : (
+        <div className="my-2 flex justify-center">No Data to Display</div>
+      )}
     </div>
   );
 };
 
-export default UserTable;
+export default PeopleTable;
--- src/components/person.tsx
+++ src/components/person.tsx
@@ -1,19 +1,29 @@
-export type Connections = {
-  relation: "friend" | "brother";
+import { ChangeEvent, useState } from "react";
+import { relations, relationsType } from "../lib";
+
+export type ConnectionsType = {
+  relation: relationsType | null;
   id: number;
 };
 
 export type PersonType = {
   name: string;
-  connections: Connections[];
+  connections: ConnectionsType[];
 };
 
 interface PersonProps {
+  id: number;
   person: PersonType;
+  people: { [id: number]: PersonType };
+  // eslint-disable-next-line no-unused-vars
+  connect: (relation: relationsType, personToId: number) => void;
   deletePerson: () => void;
 }
 
-const Person = ({ person, deletePerson }: PersonProps) => {
+const Person = ({ id, person, people, connect, deletePerson }: PersonProps) => {
+  const [connection, setConnection] = useState<ConnectionsType>({ id: -1, relation: null });
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) =>
+    setConnection({ ...connection, [e.target.name]: e.target.value });
   return (
     <div className="flex px-2 py-4 items-center justify-between">
       <div className="flex items-center">
@@ -32,6 +42,70 @@ const Person = ({ person, deletePerson }: PersonProps) => {
           </svg>
         </button>
         <p>{person.name}</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mx-2 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+        </svg>
+        <div className="flex gap-3">
+          {person.connections.map((conn) => (
+            <p key={connection.id} className="p-2 border-2 border-slate-500 rounded-2xl">
+              {people[conn.id].name} - {conn.relation?.slice(0, 2)}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <p className="flex items-center">Add Connection: </p>
+        <select
+          id="relation"
+          defaultValue=""
+          name="relation"
+          onChange={handleChange}
+          placeholder="Select Relation"
+          className="text-black px-1 py-2 border-none outline-none focus:border-slate-700"
+        >
+          <option value="" disabled>
+            Select Relation
+          </option>
+          {relations.map((type) => (
+            <option key={type} value={type} className="px-2 py-1 ">
+              {type}
+            </option>
+          ))}
+        </select>
+        <select
+          id="personTo"
+          defaultValue=""
+          name="id"
+          onChange={handleChange}
+          placeholder="Select Person"
+          className="text-black px-1 py-2 border-none outline-none focus:border-slate-700"
+        >
+          <option value="" disabled>
+            Select Person
+          </option>
+          {people &&
+            Object.entries(people)
+              .filter(([personId]) => id !== parseInt(personId, 10))
+              .map(([idTo, personTo]) => (
+                <option key={idTo} value={idTo} className="px-2 py-1 ">
+                  {personTo.name}
+                </option>
+              ))}
+        </select>
+        <button
+          type="button"
+          className="btn-contained"
+          onClick={() => connect(connection.relation, connection.id)}
+        >
+          Connect
+        </button>
       </div>
     </div>
   );
