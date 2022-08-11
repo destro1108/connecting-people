42fd9fa0d7fe0dbe26b218a5e233e74869fedee1
destro1108 <swanandev@gmail.com>
2022-08-11 17:01:03 +0530

    feat: :sparkles: added localStorage Save func

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,11 @@
 export const relations = ["Friends", "Brothers"] as const;
 
 export type relationsType = typeof relations[number] | null;
+
+export const saveToLocalStorage = (key: string, data: string) => {
+  localStorage.setItem(key, data);
+};
+
+export const fetchFromLocalStorage = (key: string) => {
+  return localStorage.getItem(key);
+};
