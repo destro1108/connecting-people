a7715c29dc5d8f0769129a8aee83db0c65197bcc
Swanand Buva <swanandev@gmail.com>
2022-08-12 16:40:44 +0530

    Revert "feat: :sparkles: added person delete functionality"
    This reverts commit bf9a05229d401fc157e881b2aa8043a695a5b970.

--- src/components/peopleTable.tsx
+++ src/components/peopleTable.tsx
@@ -1,18 +1,14 @@
-import { Dispatch } from "react";
-import { ACTIONS, ActionTypes } from "../reducer";
 import Person, { PersonType } from "./person";
 
 interface UserTableProps {
   people: { [key: string]: PersonType };
-  dispatch: Dispatch<ActionTypes>;
 }
 
-const UserTable = ({ people, dispatch }: UserTableProps) => {
-  const deletePerson = (id: number) => dispatch({ type: ACTIONS.REMOVE_USER, payload: id });
+const UserTable = ({ people }: UserTableProps) => {
   return (
-    <div className="mt-6 px-2 flex flex-col border-2 border-slate-700 gap-0 divide-y-2 divide-slate-700">
+    <div className="mt-6 p-2 flex flex-col border-2 border-slate-700 gap-0 divide-y-2 divide-slate-700">
       {Object.entries(people).map(([id, person]) => (
-        <Person key={id} person={person} deletePerson={() => deletePerson(parseInt(id, 10))} />
+        <Person key={id} person={person} />
       ))}
     </div>
   );
