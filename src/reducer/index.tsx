f040f77d61911e137503124298667002f4c6e7e5
Swanand Buva <swanandev@gmail.com>
2022-08-12 16:40:44 +0530

    Revert "feat: :sparkles: added actions to reducer"
    This reverts commit 1033d651d576319a2c11a0365ad51c5eb5331881.

--- src/reducer/index.tsx
+++ src/reducer/index.tsx
@@ -1,11 +1,7 @@
 import { PersonType } from "../components/person";
 
-// eslint-disable-next-line no-shadow
-export const ACTIONS = {
+export const ACTIONS: { [key: string]: string } = {
   ADD_USER: "ADD_USER",
-  REMOVE_USER: "REMOVE_USER",
-  SET_FROM: "SET_FROM",
-  SET_TO: "SET_TO",
 };
 
 export interface stateType {
@@ -25,41 +21,11 @@ export const initialState: stateType = {
   to: null,
 };
 
-type ActionAddUser = {
-  type: typeof ACTIONS.ADD_USER;
-  payload: { id: number; person: PersonType };
-};
-
-type ActionRemoveUser = {
-  type: typeof ACTIONS.REMOVE_USER;
-  payload: number;
-};
-type ActionSetFrom = {
-  type: typeof ACTIONS.SET_FROM;
-  payload: number;
-};
-type ActionSetTo = {
-  type: typeof ACTIONS.SET_TO;
-  payload: number;
-};
-
-export type ActionTypes = ActionAddUser | ActionRemoveUser | ActionSetFrom | ActionSetTo;
-
 // eslint-disable-next-line default-param-last
 export default (state: stateType, action: actionType) => {
-  const { type, payload } = action;
-  let people;
-  switch (type) {
+  switch (action.type) {
     case ACTIONS.ADD_USER:
-      return { ...state, people: { ...state.people, [payload.id]: payload.person } };
-    case ACTIONS.REMOVE_USER:
-      people = { ...state.people };
-      delete people[payload];
-      return { ...state, people };
-    case ACTIONS.SET_FROM:
-      return { ...state, from: payload };
-    case ACTIONS.SET_TO:
-      return { ...state, to: payload };
+      return { ...state, people: { ...state.people, [action.payload.id]: action.payload.person } };
     default:
       return state;
   }
