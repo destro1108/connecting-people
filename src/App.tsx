88cacaea7494de4181b6c538158785cd49108b27
Swanand Buva <swanandev@gmail.com>
2022-08-12 16:40:44 +0530

    Revert "feat: :art: add user btn, load sample button"
    This reverts commit 7e2c068489a7059df186aea23e5c7761f7cb11e1.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 const App = () => {
-  return (
-    <div className="container mx-auto h-full w-screen bg-outer-space-900 text-gray-200 font-Ubuntu">
-      <div className="w-full flex items-center gap-8 mt-20 px-3 py-2">
-        <div className="btn-outlined">Load Sample Data</div>
-        <div className="btn-contained">Add User</div>
-      </div>
-    </div>
-  );
+  return <div className="mx-auto container h-screen w-screen">Initial</div>;
 };
 
 export default App;
