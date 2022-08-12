a959173277e08d24aae9ee677eb80b59947d9927
Swanand Buva <swanandev@gmail.com>
2022-08-12 16:40:44 +0530

    Revert "feat: :sparkles: added localStorage Save func"
    This reverts commit 42fd9fa0d7fe0dbe26b218a5e233e74869fedee1.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,3 @@
 export const relations = ["Friends", "Brothers"] as const;
 
 export type relationsType = typeof relations[number] | null;
-
-export const saveToLocalStorage = (key: string, data: string) => {
-  localStorage.setItem(key, data);
-};
-
-export const fetchFromLocalStorage = (key: string) => {
-  return localStorage.getItem(key);
-};
