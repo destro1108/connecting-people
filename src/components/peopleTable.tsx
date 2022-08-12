d6775b8d6e72c97748928347bf2c4b0d1e1b8044
Swanand Buva <swanandev@gmail.com>
2022-08-12 16:40:44 +0530

    Revert "feat: :sparkles: connecting people"
    This reverts commit 1f26cb0497c2cfb11c1ff4f4ebc413f0fc000458.

--- src/components/peopleTable.tsx
+++ src/components/peopleTable.tsx
@@ -1,58 +1,21 @@
 import { Dispatch } from "react";
-import { relationsType } from "../lib";
 import { ACTIONS, ActionTypes } from "../reducer";
 import Person, { PersonType } from "./person";
 
-interface PeopleTableProps {
-  people: { [key: number]: PersonType };
+interface UserTableProps {
+  people: { [key: string]: PersonType };
   dispatch: Dispatch<ActionTypes>;
 }
 
-const PeopleTable = ({ people, dispatch }: PeopleTableProps) => {
+const UserTable = ({ people, dispatch }: UserTableProps) => {
   const deletePerson = (id: number) => dispatch({ type: ACTIONS.REMOVE_USER, payload: id });
   return (
     <div className="mt-6 px-2 flex flex-col border-2 border-slate-700 gap-0 divide-y-2 divide-slate-700">
-      {Object.keys(people).length ? (
-        Object.entries(people).map(([id, person]) => (
-          <Person
-            key={id}
-            id={parseInt(id, 10)}
-            people={people}
-            person={person}
-            deletePerson={() => deletePerson(parseInt(id, 10))}
-            connect={(relation: relationsType, personToId: number) => {
-              if (person.connections.some((conn) => conn.id === personToId)) return;
-              dispatch({
-                type: ACTIONS.UPDATE_USER,
-                payload: {
-                  id: parseInt(id, 10),
-                  person: {
-                    ...person,
-                    connections: [...person.connections, { relation, id: personToId }],
-                  },
-                },
-              });
-              dispatch({
-                type: ACTIONS.UPDATE_USER,
-                payload: {
-                  id: personToId,
-                  person: {
-                    ...people[personToId],
-                    connections: [
-                      ...people[personToId].connections,
-                      { relation, id: parseInt(id, 10) },
-                    ],
-                  },
-                },
-              });
-            }}
-          />
-        ))
-      ) : (
-        <div className="my-2 flex justify-center">No Data to Display</div>
-      )}
+      {Object.entries(people).map(([id, person]) => (
+        <Person key={id} person={person} deletePerson={() => deletePerson(parseInt(id, 10))} />
+      ))}
     </div>
   );
 };
 
-export default PeopleTable;
+export default UserTable;
