d042a5c9b5a96c164af77c82dc19089b242f1ccd
destro1108 <swanandev@gmail.com>
2022-08-12 13:41:19 +0530

    moved delete button to right side

--- src/components/person.tsx
+++ src/components/person.tsx
@@ -38,22 +38,8 @@ const Person = ({ id, person, people, connect, deletePerson }: PersonProps) => {
     connect(connection.relation, connection.id);
   };
   return (
-    <div className="px-2 py-4 flex md:flex-row flex-col md:items-center justify-between">
+    <div className="px-2 py-4 flex lg:flex-row flex-col lg:items-center justify-between">
       <div className="flex items-center my-2">
-        <button
-          type="button"
-          onClick={deletePerson}
-          className=" text-red-600 pr-4 cursor-pointer hover:text-red-500"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M11 6a3 3 0 11-6 0 3 3 0 016 0zM14 17a6 6 0 00-12 0h12zM13 8a1 1 0 100 2h4a1 1 0 100-2h-4z" />
-          </svg>
-        </button>
         <p>{person.name}</p>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -128,6 +114,20 @@ const Person = ({ id, person, people, connect, deletePerson }: PersonProps) => {
             />
           </svg>
           <span className="hidden md:flex">Connect</span>
+        </button>
+        <button
+          type="button"
+          onClick={deletePerson}
+          className=" text-red-600 cursor-pointer hover:text-red-500 btn-outlined"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M11 6a3 3 0 11-6 0 3 3 0 016 0zM14 17a6 6 0 00-12 0h12zM13 8a1 1 0 100 2h4a1 1 0 100-2h-4z" />
+          </svg>
         </button>
       </div>
     </div>
